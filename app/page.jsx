c92219da133411a93a16cfa4add8b5388096844e
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-04-14 11:54:30 +0000

    Add SignUp form

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,36 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-[90vh] grid grid-cols-1 md:grid-cols-2 px-3 ">
+      <div className="flex items-center">
+        <h1 className="text-3xl md:text-5xl font-light">
+          Lumeo &mdash; Create Engaging Thumbnails Using GenAI
+        </h1>
+      </div>
+      <div className="flex items-center ">
+        <form
+          action=""
+          method="POST"
+          className="w-full flex flex-col items-start"
+        >
+          <input
+            type="text"
+            placeholder="Username"
+            className="input"
+            required
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input"
+            required
+          />
+          <br />
+          <button type="submit" className="btn">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
