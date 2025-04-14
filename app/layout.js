a6cb9bcbc3697ba8bec265490fd8350f80c0cb0c
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2025-04-14 11:38:31 +0000

    add tailwind + daisyui

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,6 @@
+import "./global.css";
+import Header from "@/components/Header";
+
 export const metadata = {
   title: "Lumeo",
   description: "AI Image Thumnail Generator",
@@ -6,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,3 @@
-export default function Page() {
-  return <div>Hello there</div>;
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
